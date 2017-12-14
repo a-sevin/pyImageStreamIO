5fb2564459f0c055a478762c1c2cdf4cbdb8b9f0
sevin <Arnaud.Sevin@obspm.fr>
2017-12-14 23:08:30 +0100

    update to handle read/write on shm

--- pyImageStreamIO.cpp
+++ pyImageStreamIO.cpp
@@ -41,10 +41,12 @@ struct MyDatatype {
     Type atype;
     uint8_t asize;
 
-    MyDatatype(Type atype) : atype(atype), asize(Size[atype]) { };
+    MyDatatype(uint8_t atype) : atype(static_cast<MyDatatype::Type>(atype)), 
+                                asize(Size[atype]) { };
 };
 
 const std::vector<uint8_t> MyDatatype::Size({
+        0,
         SIZEOF_DATATYPE_UINT8,
         SIZEOF_DATATYPE_INT8,
         SIZEOF_DATATYPE_UINT16,
@@ -74,7 +76,7 @@ std::string MydatatypeToPyFormat(MyDatatype dt){
         case MyDatatype::Type::DOUBLE: return py::format_descriptor<double>::format();
         // case MyDatatype::Type::COMPLEX_FLOAT: return py::format_descriptor<(std::complex<float>>::format();
         // case MyDatatype::Type::COMPLEX_DOUBLE: return py::format_descriptor<(std::complex<double>>::format();
-        default: throw "Not implemented";
+        default: throw std::invalid_argument("Not implemented");
     }
 }
 
@@ -101,14 +103,20 @@ MyDatatype PyFormatToMydatatype(std::string pf){
         return MyDatatype::Type::DOUBLE;
     // case MyDatatype::Type::COMPLEX_FLOAT: return py::format_descriptor<(std::complex<float>>::format();
     // case MyDatatype::Type::COMPLEX_DOUBLE: return py::format_descriptor<(std::complex<double>>::format();
-    throw "Not implemented";
+    throw std::invalid_argument("Not implemented");
 }
 
 // Expose classes and methods to Python
 PYBIND11_MODULE(pyImageStreamIO, m) {
     m.doc() = "";
 
-    py::class_<MyDatatype> mydatatype(m, "Datatype");
+    auto mydatatype = py::class_<MyDatatype> (m, "Datatype")
+        .def(py::init([](uint8_t atype) {
+            return std::unique_ptr<MyDatatype>(new MyDatatype(atype));
+        }))
+        .def_readonly("size", &MyDatatype::asize)
+        .def_readonly("type", &MyDatatype::atype);
+
     py::enum_<MyDatatype::Type>(mydatatype, "Type")
         .value("UINT8", MyDatatype::Type::UINT8)
         .value("INT8", MyDatatype::Type::INT8)
@@ -132,9 +140,9 @@ PYBIND11_MODULE(pyImageStreamIO, m) {
         }))
         .def(py::init([](std::string name, int64_t numl, std::string comment) {
             if(name.size()>16)
-                throw "name too long";
+                throw std::invalid_argument("name too long");
             if(comment.size()>80)
-                throw "comment too long";
+                throw std::invalid_argument("comment too long");
             auto kw = std::unique_ptr<IMAGE_KEYWORD>(new IMAGE_KEYWORD());
             std::copy(name.begin(), name.end(), kw->name);
             kw->type = 'L';
@@ -146,9 +154,9 @@ PYBIND11_MODULE(pyImageStreamIO, m) {
            py::arg("comment") = "")
         .def(py::init([](std::string name, double numf, std::string comment) {
             if(name.size()>16)
-                throw "name too long";
+                throw std::invalid_argument("name too long");
             if(comment.size()>80)
-                throw "comment too long";
+                throw std::invalid_argument("comment too long");
             auto kw = std::unique_ptr<IMAGE_KEYWORD>(new IMAGE_KEYWORD());
             std::copy(name.begin(), name.end(), kw->name);
             kw->type = 'D';
@@ -160,11 +168,11 @@ PYBIND11_MODULE(pyImageStreamIO, m) {
            py::arg("comment") = "")
         .def(py::init([](std::string name, std::string valstr, std::string comment) {
             if(name.size()>16)
-                throw "name too long";
+                throw std::invalid_argument("name too long");
             if(valstr.size()>16)
-                throw "valstr too long";
+                throw std::invalid_argument("valstr too long");
             if(comment.size()>80)
-                throw "comment too long";
+                throw std::invalid_argument("comment too long");
             auto kw = std::unique_ptr<IMAGE_KEYWORD>(new IMAGE_KEYWORD());
             std::copy(name.begin(), name.end(), kw->name);
             kw->type = 'S';
@@ -195,9 +203,19 @@ PYBIND11_MODULE(pyImageStreamIO, m) {
         // }))
         .def_readonly("name", &IMAGE_METADATA::name)
         .def_readonly("naxis", &IMAGE_METADATA::naxis)
-        .def_readonly("size", &IMAGE_METADATA::size)
+        .def_property_readonly("size", [](const IMAGE_METADATA &md){
+            std::vector<uint32_t> dims(md.naxis);
+            const uint32_t *ptr = md.size;
+            for(auto &&dim: dims){
+                dim = *ptr;
+                ++ptr;
+            }
+            return dims;
+        })
         .def_readonly("nelement", &IMAGE_METADATA::nelement)
-        .def_readonly("atype", &IMAGE_METADATA::atype)
+        .def_property_readonly("atype", [](const IMAGE_METADATA &md){
+            return MyDatatype(md.atype).atype;
+        })
         .def_readonly("creation_time", &IMAGE_METADATA::creation_time)
         .def_readonly("last_access", &IMAGE_METADATA::last_access)
         .def_readonly("shared", &IMAGE_METADATA::shared)
@@ -218,10 +236,9 @@ PYBIND11_MODULE(pyImageStreamIO, m) {
         .def_readonly("used", &IMAGE::used)
         .def_readonly("memsize", &IMAGE::memsize)
         .def_readonly("md", &IMAGE::md)
-        // .def_readonly("array", &IMAGE::array)  // TODO handle union
         .def_readonly("kw", &IMAGE::kw)
-        .def_buffer([](IMAGE &img) -> py::buffer_info {
-            MyDatatype dt(static_cast<MyDatatype::Type>(img.md->atype));
+        .def_buffer([](const IMAGE &img) -> py::buffer_info {
+            MyDatatype dt(img.md->atype);
             std::string format = MydatatypeToPyFormat(dt);
             std::vector<ssize_t> shape(img.md->naxis);
             std::vector<ssize_t> strides(img.md->naxis);
@@ -231,7 +248,6 @@ PYBIND11_MODULE(pyImageStreamIO, m) {
                 strides[axis] = stride;
                 stride *= shape[axis];
             }
-
             return py::buffer_info(
                     img.array.UI8,  /* Pointer to buffer */
                     dt.asize,       /* Size of one scalar */
@@ -241,18 +257,54 @@ PYBIND11_MODULE(pyImageStreamIO, m) {
                     strides         /* Strides (in bytes) for each index */
                 );
 
-        });;
+        })
+        .def("write", [](IMAGE &img, py::buffer b){
+            /* Request a buffer descriptor from Python */
+            py::buffer_info info = b.request();
 
-    m.def("createIm", [](std::string name, py::buffer b, uint8_t shared, uint16_t NBkw) {
+            if(img.md->atype != PyFormatToMydatatype(info.format).atype)
+                throw std::invalid_argument("incompatible type");
+            if(info.ndim != img.md->naxis)
+                throw std::invalid_argument("incompatible number of axis");
+            const uint32_t *size_ptr = img.md->size;
+            for(auto &dim: info.shape){
+                if (*size_ptr != dim)
+                    throw std::invalid_argument("incompatible shape");
+                ++size_ptr;
+            }
+
+            MyDatatype dt(img.md->atype);
+            uint8_t *buffer_ptr = (uint8_t *)info.ptr;
+            uint64_t size = img.md->nelement * dt.asize;             
+
+            img.md->write = 1; // set this flag to 1 when writing data
+            std::copy(buffer_ptr, buffer_ptr+size, img.array.UI8);
+
+            std::vector<uint32_t> ushape(info.ndim);
+            std::copy(info.shape.begin(), info.shape.end(), ushape.begin());                
+    		ImageStreamIO_sempost(&img, -1);
+            img.md->write = 0; // Done writing data
+            img.md->cnt0++;
+            img.md->cnt1++;
+
+        }, R"pbdoc(
+          Write into memory image stream
+
+          Parameters:
+            buffer [in]:  buffer to put into memory image stream
+          )pbdoc",
+          py::arg("buffer"));
+
+    m.def("create", [](std::string name, py::array_t<uint32_t> b, uint8_t atype, uint8_t shared, uint16_t NBkw) {
         /* Request a buffer descriptor from Python */
         py::buffer_info info = b.request();
 
-        uint8_t atype = PyFormatToMydatatype(info.format).atype;
-        std::vector<uint32_t> ushape(info.ndim);
-        std::copy(info.shape.begin(), info.shape.end(), ushape.begin());                
+        // uint8_t atype = PyFormatToMydatatype(info.format).atype;
+        // std::vector<uint32_t> ushape(info.ndim);
+        // std::copy(info.shape.begin(), info.shape.end(), ushape.begin());                
 
         auto img = std::unique_ptr<IMAGE>(new IMAGE());
-        ImageStreamIO_createIm(img.get(), name.c_str(), info.ndim, ushape.data(), atype, shared, NBkw);
+        ImageStreamIO_createIm(img.get(), name.c_str(), info.size, (uint32_t*)info.ptr, atype, shared, NBkw);
 
         return img;
     }, R"pbdoc(
@@ -260,16 +312,31 @@ PYBIND11_MODULE(pyImageStreamIO, m) {
 
           Parameters:
             name   [in]:  the name of the shared memory file will be SHAREDMEMDIR/<name>_im.shm
-            buffer [in]:  np.array of the image.
+            dims   [in]:  np.array of the image.
+            atype  [in]:  data type code, pyImageStreamIO.Datatype
             shared [in]:  if true then a shared memory buffer is allocated.  If false, only local storage is used.
             NBkw   [in]:  the number of keywords to allocate.
+          Return:
+            image  [out]: Image Buffer
           )pbdoc",
           py::arg("name"),
           py::arg("buffer"),
+          py::arg("atype") = MyDatatype::Type::FLOAT,
           py::arg("shared") = 1,
           py::arg("NBkw") = 1
           );
-    m.def("read_sharedmem_image_toIMAGE", &ImageStreamIO_read_sharedmem_image_toIMAGE,
-          "Read / connect to existing shared memory image stream");
+          
+    m.def("read", [](std::string name) {
+        auto img = std::unique_ptr<IMAGE>(new IMAGE());
+        ImageStreamIO_read_sharedmem_image_toIMAGE(name.c_str(), img.get());
+        return img;        
+    }, R"pbdoc(
+          Read / connect to existing shared memory image stream
 
+          Parameters:
+            name   [in]:  the name of the shared memory file to connect
+          Return:
+            image  [out]: Image Buffer
+          )pbdoc",
+          py::arg("name"));
 }
